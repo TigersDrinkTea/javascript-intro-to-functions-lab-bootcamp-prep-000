47b5f6a209661080039615e1a02acd84889258e0
brainy-language-7409 <castlesofcode@gmail.com>
2019-02-06 20:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
- function sayHiToGrandma(string){
-   if (string.toUpperCase() ===  string)
+function sayHiToGrandma (string) {
+   if (string.toUpperCase() === string) {
+     return "YES INDEED !"
+   }
+   else if (string === "I love you, Grandma.")
+   return "I love you, too."
+ }
+ else {
+   return ("I can't hear you!")
  }
  
